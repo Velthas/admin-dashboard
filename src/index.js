5e85979b4a7ce6738adb5b86e3e5e6a311f23f72
Velthas <damianobattaglia97@gmail.com>
2022-07-26 22:55:56 +0200

    created upper side of header with small animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import stylesheet
 import './style.css';
 
-// Images
+// Sidebar Images
 import ballot from './images/ballot.svg';
 import comment from './images/comments.svg';
 import home from './images/home.svg';
@@ -13,7 +13,13 @@ import user from './images/user.svg';
 import userAlt from './images/users-alt.svg';
 import logo from './images/interface.svg';
 
-// Gather all images based on location in the grid
+//Header Images
+import noBell from './images/bell-no.svg';
+import yesBell from './images/bell-yes.svg';
+import search from './images/search.svg';
+import birb from './images/birb.jpg';
+
+// Gather all images in the sidebar based on location in the grid
 const images = [logo, home, user, comment, past, ballot, userAlt, settings, help, protection];
 
 // Get all the image elements inside the sidebar
@@ -23,6 +29,41 @@ const sidebarIcons = Array.from(document.querySelectorAll('.sidebar img'));
 for(let i = 0; i < sidebarIcons.length; i++) {
     sidebarIcons[i].src = images[i];
 }
+
+// Gather all images in the header
+const headerImages = [search, noBell, birb];
+
+// Gather all the images element in the hader
+const headerImgs = Array.from(document.querySelectorAll('.header img'));
+
+// Change their source dinamically
+for(let i = 0; i < headerImages.length; i++) {
+    headerImgs[i].src = headerImages[i];
+}
+
+
+
+
+
+
+
+// Add tiny animation for fun
+// There are two images in, this will only get the notification bell
+const bellImg = document.querySelector('.user-bar img');
+
+// Counter
+let counter = 0;
+
+bellImg.addEventListener('click', function () {
+    if(counter === 0) {
+        bellImg.src = yesBell;
+        counter = 1; 
+    } else {
+        bellImg.src = noBell;
+        counter = 0;
+    }
+    
+});
 
 
 
